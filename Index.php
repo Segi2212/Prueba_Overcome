fad13ba60812b9c9d47c1188493bff7725b5183d
segi2212 <arielromram@gmail.com>
2022-11-17 18:26:30 -0600

    Login

---
+++ Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <div class="cont">
+        <div class="contenedor">
+            <p class="titulo">Bienvenida/o</p>
+            <input class="caja" type="text" placeholder="Usuario">
+            <input class="caja" type="password" placeholder="Contraseña">
+            <a href="">
+                <div class="boton">Ingresar</div>
+            </a>
+            <p class="recuperar">¿Aún no tienes una cuenta? <a href="">Crea una</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
